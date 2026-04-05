cdd5864b53ffaf9e09910030d49fc2aa3f4886bd
cyb3rgh05t <cyb3rgh05t_01@proton.me>
2026-04-05 15:11:10 +0200

    cache busting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
+  },
   server: {
     port: 5173,
     proxy: {
